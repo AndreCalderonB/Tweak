86fb4a3659b960b4269a2fee4c406f7d1f992bc0
Juan Cazarez <a00570574@itesm.mx>
2021-03-19 12:02:16 -0600

    Followers and details per user
    Creation of followers, following and details for each user

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,42 @@
 const mongoose = require('mongoose');
 
+const detailSchema = new mongoose.Schema({
+  weight: {
+    type: Number,
+    required: true
+  },
+  height: {
+    type: Number,
+    required: true
+  },
+  birthdate: {
+    type: Date,
+    required: true
+  },
+  mainSport: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Sport'
+  },
+  team: {
+    type: String,
+    required: true
+  },
+  position: {
+    type: String,
+    required: true
+  },
+  localization: {
+    state: {
+      type: String,
+      required: true
+    },
+    country: {
+      type: String,
+      required: true
+    }
+  }
+})
+
 //------------ User Schema ------------//
 const UserSchema = new mongoose.Schema({
   name: {
@@ -25,6 +62,17 @@ const UserSchema = new mongoose.Schema({
   resetLink: {
     type: String,
     default: ''
+  },
+  followers: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  following: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  details: {
+    type: detailSchema
   }
 }, { timestamps: true });
 
--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const User = require('../models/User');
 
 const userRouter = express.Router();
 
-const userController = require('../controllers/userController')
+const userController = require('../controllers/userController');
+const { Error } = require('mongoose');
 
 userRouter.use(express.json());
 
@@ -11,5 +13,74 @@ userRouter.get('/index/:id', userController.showAllHandle);
 
 //Show
 userRouter.get('/show/:id', userController.showHandle);
+
+
+//Show All users with details
+userRouter.route('/')
+.get((req,res,next) => {
+    User.find({})
+    .then((users) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(users);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+
+//User details - Muestra seguidores, seguidos y detalles de usuario
+userRouter.route('/details/:userId')
+.get((req,res,next) => {
+    User.findOne({_id: req.params.userId})
+    .populate('followers')
+    .populate('following')
+    .then((user) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(user.details);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.post((req, res, next) => {
+    User.findOne(req.params.id)
+    .then((user) => {
+        if(req.body != null){
+            const details = req.body;
+            user.details = details;
+            user.save();
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(user);
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+
+//Follow user
+userRouter.route('/:userId/follow/:followerId')
+.post((req, res, next) => {
+    User.findOne({_id: req.params.userId})
+    .then((user) => {
+        User.findOne({_id: req.params.followerId})
+        .then((follower) => {
+            if(user.following.includes(follower)){
+                user.following.push(follower);
+                follower.followers.push(user);
+                user.save();
+                follower.save();
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(user);
+            }else{
+                err = new Error('You already follow that user');
+                err.status = 401;
+                return next(err);
+            }
+        })  
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+
+
+
 
 module.exports = userRouter;
